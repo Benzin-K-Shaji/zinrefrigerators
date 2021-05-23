a9846b8c65a56ab013f50020fd8f9f4d7360b297
Benzin-K-Shaji <mca512@rajagiri.edu>
2021-05-23 22:29:51 +0530

    m

--- resources/views/vieworders.blade.php
+++ resources/views/vieworders.blade.php
@@ -9,11 +9,18 @@
 </head>
 
 <body background="https://i.pinimg.com/originals/59/e7/58/59e758cd94b6a989aa2cc7b4eeb68098.jpg">
+<style type="text/css">
+@media print {
+    #hide {
+        display :  none;
+    }
+}
+</style>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <img src="https://images.squarespace-cdn.com/content/v1/554834bbe4b08ea580f5ce5d/1472174878951-3P9ZELBBRTOBQWYO6MDP/ke17ZwdGBToddI8pDm48kPV_8t8qYDH_v8mc-92omtJZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7Xj1nVWs2aaTtWBneO2WM-sHxhTq-bNHvWHXpBeMx1mdq9a3VILVIsn_vpTFrxe7vw/Zin-Logo-7-9-13.jpg" height="45" alt="" >
-          <a class="navbar-brand" href="/ah">HOME</a>
+          <a class="navbar-brand" href="/ah" id="hide">HOME</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -33,12 +40,14 @@
             
             </ul>
             <form class="d-flex">
-             
+
               <a href="/logout" class="btn btn-primary">Logout</a>
+
             </form>
           </div>
         </div>
       </nav>
+      <div style="text-align:right"><button id="hide" class="btn btn-outline-warning" onclick="window.print()">Print</button></div>
       
 <br>
 <div class="container">
@@ -67,7 +76,8 @@
     <td>{{$prods->address}}</td>
     <td>{{$prods->status}}</td>
     <td>{{$prods->paymentmethod}}</td>
-    <td><a class="btn btn-warning"  href="/order/{{$prods->id}}/edit" >UPDATE</a></td>
+    <td><a class="btn btn-warning"  href="/order/{{$prods->id}}/edit" id="hide" >UPDATE</a></td>
+
     
 </tr>
 
@@ -78,7 +88,6 @@
 </div>
 </div>
 </div>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 </html>
